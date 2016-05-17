c4b72120d60fa31416e79b84534efa322fc0e8de
Aveletsky <ixus.van.axel@gmail.com>
2016-05-17 15:15:09 +0300

    add category

--- ClientSide/src/app/category/category.controller.js
+++ ClientSide/src/app/category/category.controller.js
@@ -9,29 +9,26 @@
   function CategoryController() {
     var vm = this;
 
-    var crudServiceBaseUrl = "//localhost:1337/api/category";
+    var crudServiceBaseUrl = "//localhost:1337/api/category/";
 
     vm.treelistOptions = {
       dataSource: {
         transport: {
           read:  {
-            url: crudServiceBaseUrl + "/all",
-            dataType: "jsonp"
+            url: crudServiceBaseUrl,
+            dataType: "json"
           },
-          ///EmployeeDirectory/Update
           update: {
-            url: crudServiceBaseUrl + "/",
-            dataType: "jsonp"
+            url: crudServiceBaseUrl,
+            dataType: "json"
           },
-          ////EmployeeDirectory/Destroy
           destroy: {
-            url: crudServiceBaseUrl + "/",
-            dataType: "jsonp"
+            url: crudServiceBaseUrl,
+            dataType: "json"
           },
-          ////EmployeeDirectory/Create
           create: {
-            url: crudServiceBaseUrl + "/",
-            dataType: "jsonp"
+            url: crudServiceBaseUrl,
+            dataType: "json"
           },
           parameterMap: function(options, operation) {
             if (operation !== "read" && options.models) {
@@ -44,14 +41,7 @@
             id: "_id",
             parentId: "parent_id",
             fields: {
-              __v:{nullable:true},
-              _id: { type: "string", editable: false},
-              name: { type: "string", validation: { required: true } },
-              description:{type:"string", nullable:true},
-              created_date: { type: "date" },
-              updated_date: { type: "date" },
-              active: {type:"boolean"},
-              parent_id: { nullable: true, type: "string"}
+              name: {field: "name", type: "string", validation: { required: true } }
             }
           }
         }
@@ -59,17 +49,13 @@
       sortable: true,
       editable: true,
       columns: [
-        { field: "_id", title: "Id", width: "40px" },
         { field: "name", title: "Category name", width: "150px" },
-        { field: "description",title:"Description"},
-        { field: "created_date",title: "Created date"},
-        { field:"updated_date",title: "Updated date"},
-        { field:"active",title:"Active",
-          template: "<input type='checkbox' data-bind='checked: active' />",
-          width: "100px"
-        },
-        { command: ["edit"] }
 
+        { command: [
+          { name: "edit" },
+          { name: "createChild" },
+          { name: "destroy" }]
+        }
       ]
     };
   }
