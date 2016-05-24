d5c297d9237e1227a9f610e3c5f7b394586d62e5
Aveletsky <ixus.van.axel@gmail.com>
2016-05-24 18:36:22 +0300

    Add some custom localization labels

--- ClientSide/src/app/category/category.controller.js
+++ ClientSide/src/app/category/category.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -17,40 +17,55 @@
 
     vm.treelistOptions = {
       dataSource: DataSource.getDataSource(crudServiceBaseUrl),
-      dragAndDrop:true,
-      editable: {move:true,mode: "popup"},
+      dragAndDrop: true,
+      editable: {move: true, mode: "popup"},
       dragend: onDragEnd,
-      toolbar:["create"],
+      toolbar: [
+        {
+          name: "create",
+          text:"Добавить категорию"
+        }
+      ],
       sortable: true,
-      height:540,
+      height: 540,
       columns: [
-        { field: "name",expandable: true, title: "Название категории", width: "150px" },
-        { field:"description",title:"Описание"},
-        { field:"created_date",
-          title:"Создано",
-          format:"{0:d MMM yyyy}",
+        {field: "name", expandable: true, title: "Название категории", width: "150px"},
+        {field: "description", title: "Описание"},
+        {
+          field: "created_date",
+          title: "Создано",
+          format: "{0:d MMM yyyy}",
           width: 100,
           attributes: {
             style: "text-align: center;"
           }
         },
-        { field:"updated_date",
-          title:"Обновлено",
-          format:"{0:d MMMM yyyy, H:mm}",
+        {
+          field: "updated_date",
+          title: "Обновлено",
+          format: "{0:d MMMM yyyy, H:mm}",
           width: 150,
           attributes: {
             style: "text-align: center;"
           }
         },
-        { field:"active",
-          title:"Активно",
+        {
+          field: "active",
+          title: "Активно",
 
 
           width: 60,
           attributes: {
             style: "text-align: center;"
-          }},
-        { title: "Редактирование", command: [ "edit", "destroy" ], width: 190,
+          }
+        },
+        {
+          title: "Редактирование",
+          command: [
+            {name: "edit",text: "Править"},
+            {name: "destroy",text:"Удалить"}
+          ],
+          width: 190,
           attributes: {
             style: "text-align: center;"
           }
