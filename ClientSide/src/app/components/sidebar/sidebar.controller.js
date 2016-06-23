64e0b557595c70b65046d7f5b45bb19498dbf342
Aveletsky <ixus.van.axel@gmail.com>
2016-06-23 17:31:26 +0300

    WEB-52 Category Read categories from DB and recursively show  tree-view like aside-bar

--- ClientSide/src/app/components/sidebar/sidebar.controller.js
+++ ClientSide/src/app/components/sidebar/sidebar.controller.js
@@ -1,22 +1,58 @@
-(function (){
+(function () {
   "use strict";
 
   angular
     .module('KnowledgePortal')
-    .controller('SidebarController', SidebarController)
+    .controller('SidebarController', SidebarController, ['$scope', 'sidebarService']);
 
   /** @ngInject */
-  function SidebarController($scope) {
+  function SidebarController($scope, sidebarService) {
 
-    $scope.categories = [
-      {'name':'Йога'},
-      {'name':'Танцы'},
-      {'name':'Вокал'},
-      {'name':'Гитара'}
-    ]
+    var promise = sidebarService.getCategories();
+    promise.then(function (response) {
+      //see more
+      console.log($scope.categories = response.data);
 
+      console.log($scope.categoryMenu = makeObj());
+
+    });
+
+    function makeObj(){
+      var result = [];
+      $scope.categories.forEach(function (item) {
+        if (item.parent_id == null) {
+          var categoryObj = {
+            name: item.name,
+            id: item._id,
+            childes: []
+          };
+          result.push(categoryObj);
+        }
+      });
+      return recurFactory(result)
+    }
+
+    function recurFactory(resObj) {
+      $scope.categories.forEach(function(item){
+        if (item.parent_id){
+          resObj.forEach(function(_item,i,resObj){
+            if(item.parent_id==_item.id){
+              var _categoryObj={
+                name: item.name,
+                id: item._id,
+                childes: []
+              };
+              resObj[i].childes.push(_categoryObj);
+
+              resObj[i].childes=recurFactory(resObj[i].childes)
+            }
+          })
+        }
+
+      });
+      return resObj;
+    }
   }
 
+
 })();
-
-
--- ClientSide/src/app/index.route.js
+++ ClientSide/src/app/index.route.js
@@ -1,57 +1,69 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('KnowledgePortal')
-    .config(routerConfig);
+    angular
+      .module('KnowledgePortal')
+      .config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      })
-      .state('schedulerCourse', {
-        url: '/scheduler',
-        templateUrl: 'app/scheduler/scheduler.html',
-        controller: 'SchedulerCourseController',
-        controllerAs: 'scheduler'
-      })
-      .state('category',{
-        url: "/category",
-        templateUrl: 'app/category/category.html',
-        controller: 'CategoryController',
-        controllerAs: 'category'
-      })
-      .state('users', {
-        url: '/users',
-        templateUrl: 'app/users/users.html',
-        controller: 'UsersController',
-        controllerAs: 'users'
-
-      })
-      .state('managers', {
-        url: '/managers',
-        templateUrl: 'app/managers/managers.html',
-        controller: 'ManagersController',
-        controllerAs: 'managers'
+    /** @ngInject */
+    function routerConfig($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'app/main/main.html',
+          controller: 'MainController',
+          controllerAs: 'main'
         })
-      .state('product', {
-        url: '/product/:id',
-        templateUrl: 'app/product/product.html',
-        controller: 'ProductController',
-        controllerAs: 'product',
-        resolve: {
-          productRes: function ($stateParams,$http) {
-            return $http({method: 'GET', url: "//localhost:1337/api/product/"+$stateParams.id});
+        .state('schedulerCourse', {
+          url: '/scheduler',
+          templateUrl: 'app/scheduler/scheduler.html',
+          controller: 'SchedulerCourseController',
+          controllerAs: 'scheduler'
+        })
+
+        .state('category', {
+          url: "/category",
+          templateUrl: 'app/category/category.html',
+          controller: 'CategoryController',
+          controllerAs: 'category'
+        })
+        .state('categoryId', {
+          url: '/category/:id',
+          templateUrl: 'app/category/category.html',
+          controller: 'sidebarController',
+          controllerAs: 'categoryId',
+          resolve: {
+            categoryRes: function ($stateParams, $http) {
+              return $http({method: 'GET', url: "//localhost:1337/api/category/" + $stateParams.id});
+            }
           }
-        }
-      });
+        })
+        .state('users', {
+          url: '/users',
+          templateUrl: 'app/users/users.html',
+          controller: 'UsersController',
+          controllerAs: 'users'
+        })
 
-    $urlRouterProvider.otherwise('/');
-  }
+        .state('managers', {
+          url: '/managers',
+          templateUrl: 'app/managers/managers.html',
+          controller: 'ManagersController',
+          controllerAs: 'managers'
+        })
+        .state('product', {
+          url: '/product/:id',
+          templateUrl: 'app/product/product.html',
+          controller: 'ProductController',
+          controllerAs: 'product',
+          resolve: {
+            productRes: function ($stateParams, $http) {
+              return $http({method: 'GET', url: "//localhost:1337/api/product/" + $stateParams.id});
+            }
+          }
+        });
 
-})();
+      $urlRouterProvider.otherwise('/');
+    }
+
+  })();
