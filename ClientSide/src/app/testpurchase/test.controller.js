c4d663489af59771f139d1ca94ae69a6a51b95bb
Надежда Спорыш < >
2016-06-27 01:20:41 +0300

    created purchase directive, test

--- ClientSide/src/app/testpurchase/test.controller.js
+++ ClientSide/src/app/testpurchase/test.controller.js
@@ -5,29 +5,16 @@
     .module('KnowledgePortal')
     .controller('TestController', TestController);
 
-  function TestController(DataSource, $kWindow) {
+  function TestController(DataSource) {
     var vm = this;
-
-    vm.product = DataSource.getDataSource('//localhost:1337/api/product').read()[0];
-    vm.openWindow = function(){
-      var windowInstance = $kWindow.open({
-        options:{
-          modal: true,
-          title: "Покупка",
-          resizable: true,
-          height: 150,
-          width: 400,
-          visible: false
-        },
-        templateUrl: 'app/testpurchase/test.html',
-        controller: 'TestController',
-        resolve: {
-          product: function () {
-            vm.product
-          }
-        }
-      });
+    vm.winvisible = false;
+    vm.product = {
+      name:"йога",
+      start:"12.01",
+      end:"13.01",
+      description:"медленно меняйте позы",
+      location:"подвал",
+      cost: 200
     }
-
-    }
+  }
 })();
