133d45765b84e1f381a8afa53c628fee1d75aa86
nadiyka <sporyshnadiya@gmail.com>
2016-06-29 11:13:58 +0300

    valodation added

--- ClientSide/src/app/components/purchase/purchase.directive.js
+++ ClientSide/src/app/components/purchase/purchase.directive.js
@@ -24,14 +24,41 @@
 
   }
 
+
   function PurchaseController() {
     var vm = this;
     vm.quantity = 1;
+    vm.maxQuantity = vm.product.seats_count - vm.product.busy_seats_count;
     vm.mail = {
       buyer:"",
       gifted:""
     };
     vm.certificate = false;
+
+    vm.cancel = function() {
+      vm.modalWindow.close()
+    };
+    vm.pay = function () {
+      if (vm.validator.validate()){
+        var result = {};
+        if (vm.certificate){
+          result = {
+            sum: vm.quantity*vm.product.cost,
+            mail: vm.mail.gifted,
+            buyer:vm.mail.buyer
+          }
+        }
+        else {
+          result = {
+            sum: vm.quantity*vm.product.cost,
+            mail: vm.mail.buyer
+          };
+        }
+        vm.modalWindow.close();
+      }
+      else {
+      }
+    }
   }
 
 })();
--- ClientSide/src/app/testpurchase/test.controller.js
+++ ClientSide/src/app/testpurchase/test.controller.js
@@ -14,7 +14,9 @@
       end:"13.01",
       description:"медленно меняйте позы",
       location:"подвал",
-      cost: 200
+      cost: 200,
+      seats_count: 40,
+      busy_seats_count: 35
     }
   }
 })();
