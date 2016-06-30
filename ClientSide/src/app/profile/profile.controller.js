504ab8933a687d14d290ccde8f087936c310ae0c
nadiyka <sporyshnadiya@gmail.com>
2016-06-30 14:32:04 +0300

    correct datepicker

--- ClientSide/src/app/profile/profile.controller.js
+++ ClientSide/src/app/profile/profile.controller.js
@@ -5,21 +5,27 @@
     .controller('ProfileController', ProfileController);
 
   /** @ngInject */
-  function ProfileController() {
+  function ProfileController($http) {
     var vm = this;
-    
+    vm.crudServiceBaseUrl = "//localhost:1337/api/customer";
     vm.Gender = [
-      {Gender:"Мужской"},
-      {Gender:"Женский"}
+      {Id: 1, Name:"Мужской"},
+      {Id: 2, Name:"Женский"}
     ];
     vm.user = {
-      FirstName:"aaa",
-      LastName:"bbb",
-      Gender: "Женский",
-      BirthDate: "5/31/2016"
+      "_id":"5774fe68565af2040ec07647","LastName":"agdsfaw","FirstName":"agfdsea","Gender":"Женский","Address":"reahger","Phone":"7978000000","BirthDate":"1989-06-01T11:10:04.940Z","Email":"tq34tert@mail.ru","UpdatedDate":"2016-06-30T11:11:36.826Z","RegistrationDate":"2016-06-30T11:11:36.826Z"
     };
     vm.save = function() {
-      window.alert(vm.user.Gender);
+      window.alert("save");
+      if (vm.validator.validate()){
+        window.alert("valid");
+        $http.put(vm.crudServiceBaseUrl, vm.user).then(function (response) {
+          e.success(response.data.article);
+        })
+      }
+      else {
+        window.alert("invalid");
+      }
     }
   }
 })();
